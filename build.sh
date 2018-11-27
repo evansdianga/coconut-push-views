b4297269777ae17f3508da461eaf9ed764b7f638
Evans D <edianga@rti.org>
2018-11-27 19:47:23 +0300

    Created build process to generate the files

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+coffee -o lib -c src/index.coffee
+envVar='#! /usr/bin/env node'
+sed -i.bak "1 s~^.*$~$envVar~" 'lib/index.js' && rm 'lib/index.js.bak'
+cp lib/index.js bin/index.js
+
+# https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac
+# https://stackoverflow.com/questions/32610261/sed-error-bad-flag-in-substitute-command-u
